e6702b469186b4deda286fcd6e1e44539ce7ad7b
Mark Griffiths <mark@thebespokepixel.com>
2016-04-24 07:49:36 +0100

    Snapshot term-ng 0.6.1 ∆11

--- gulpfile.js
+++ gulpfile.js
@@ -6,34 +6,17 @@
 const gulp = require('gulp')
 const cordial = require('@thebespokepixel/cordial')
 
-// Default
-gulp.task('default', ['babel-format'])
-
-// Versioning
-gulp.task('inc-build', cordial.version.build.inc)
-gulp.task('reset-build', cordial.version.build.reset)
-gulp.task('bump', ['inc-build'], cordial.version.build.write)
-gulp.task('reset', ['reset-build'], cordial.version.build.write)
-
 // Comtranspilationatting
-gulp.task('babel-format', ['bump'], cordial.format.babel(['src/**/*.js'], './'))
+gulp.task('babel-format', cordial.format.babel(['src/**/*.js'], './'))
 
 // Tests
-gulp.task('test', ['xo'], cordial.test.ava(['test/*.js']))
-gulp.task('xo', cordial.test.xo(['**/*.js', '!{scripts,node_modules}/**/*']))
-
-// npm
-gulp.task('publish', cordial.npm.publish)
-
-// Git
-gulp.task('commit', cordial.git.commitAll)
-gulp.task('push', cordial.git.pushAll('origin'))
-gulp.task('backup', ['push'], cordial.git.pushAll('backup'))
-gulp.task('publish-push', ['publish'], cordial.git.pushAll('origin'))
-gulp.task('publish-backup', ['publish-push'], cordial.git.pushAll('backup'))
+gulp.task('ava', cordial.test.ava(['test/*.js']))
+gulp.task('xo', cordial.test.xo(['**/*.js', '!{node_modules}/**/*']))
+gulp.task('test', gulp.parallel('xo', 'ava'))
 
 // Guppy Hooks
-gulp.task('post-flow-release-start', ['reset', 'test'], cordial.flow.release.start)
-gulp.task('post-flow-release-finish', ['publish-push'])
-gulp.task('filter-flow-release-start-version', cordial.flow.release.versionFilter)
-gulp.task('filter-flow-release-finish-tag-message', cordial.flow.release.tagFilter)
+gulp.task('start-release', gulp.series('reset', 'test'))
+gulp.task('finish-release', gulp.series('backup'))
+
+// Default
+gulp.task('default', gulp.series('bump', 'babel-format'))
